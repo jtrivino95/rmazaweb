15d81cb1ca226e53b694e1ecbcc92f1f5c4f13fb
Javier Trivino <Javier.Trivino@trivago.com>
2017-11-12 21:12:10 +0100

    Added Vagrant support

--- rmazaweb/settings/production.py
+++ rmazaweb/settings/production.py
@@ -1,8 +1,50 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+import random
+import string
+
 from .base import *
 
+
 DEBUG = False
+
+# DJANGO_SECRET_KEY *should* be specified in the environment. If it's not, generate an ephemeral key.
+if 'DJANGO_SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+else:
+    # Use if/else rather than a default value to avoid calculating this if we don't need it
+    print("WARNING: DJANGO_SECRET_KEY not found in os.environ. Generating ephemeral SECRET_KEY.")
+    SECRET_KEY = ''.join([random.SystemRandom().choice(string.printable) for i in range(50)])
+
+# Redirect all requests to HTTPS
+SECURE_SSL_REDIRECT = os.getenv('DJANGO_SECURE_SSL_REDIRECT', 'off') == 'on'
+
+# Accept all hostnames, since we don't know in advance which hostname will be used for any given Heroku instance.
+# IMPORTANT: Set this to a real hostname when using this in production!
+# See https://docs.djangoproject.com/en/1.10/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(';')
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# BASE_URL required for notification emails
+BASE_URL = 'http://' + os.environ['BASE_URL'] + ':8000'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
 
 try:
     from .local import *
