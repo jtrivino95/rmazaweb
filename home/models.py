229f6ccc05850176e7392b051afa6bc4657d62c9
Javier Trivino <javier.trivino@trivago.com>
2017-11-19 12:59:09 +0100

    FormPage now in home/models

--- home/models.py
+++ home/models.py
@@ -2,23 +2,50 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from base.models import FormPage
 from blog.models import OfferPage
 
+from modelcluster.fields import ParentalKey
+
 from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+
 )
 
 
-class HomePage(FormPage):
+class HomePage(AbstractEmailForm):
     about = RichTextField()
+    thank_you_text = RichTextField()
 
-    content_panels = FormPage.content_panels + [
+    content_panels = AbstractEmailForm.content_panels + [
         FieldPanel('about'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
     ]
 
     def get_context(self, request, *args, **kwargs):
         context = super(HomePage, self).get_context(request, *args, **kwargs)
         context['offers']  = OfferPage.objects.filter(live=True)
         return context
+
+class FormField(AbstractFormField):
+    """
+    Wagtailforms is a module to introduce simple forms on a Wagtail site. It
+    isn't intended as a replacement to Django's form support but as a quick way
+    to generate a general purpose data-collection form or contact form
+    without having to write code. We use it on the site for a contact form. You
+    can read more about Wagtail forms at:
+    http://docs.wagtail.io/en/latest/reference/contrib/forms/index.html
+    """
+    page = ParentalKey('HomePage', related_name='form_fields')
