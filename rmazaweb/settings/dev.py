95300add97961a7623bd12ed7f79085a0465304e
Javier Trivino <javier.trivino@trivago.com>
2017-11-19 19:59:12 +0100

    Updated settings

--- rmazaweb/settings/dev.py
+++ rmazaweb/settings/dev.py
@@ -1,6 +1,5 @@
-from __future__ import absolute_import, unicode_literals
-
 from .base import *
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -8,4 +7,15 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '7pepm9wkl7d=bpbrych*o$k$&qrnha+f6_l9uk#lkguv*y0_on'
 
+# Default to dummy email backend. Configure dev/production/local backend
+# as per https://docs.djangoproject.com/en/dev/topics/email/#email-backends
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- rmazaweb/settings/production.py
+++ rmazaweb/settings/production.py
@@ -1,50 +1,33 @@
-from __future__ import absolute_import, unicode_literals
+from .base import *
 
-import os
 import random
 import string
 
-from .base import *
 
-
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# DJANGO_SECRET_KEY *should* be specified in the environment. If it's not, generate an ephemeral key.
-
+# DJANGO_SECRET_KEY *should* be specified in the environment. If it's not, generate an ephemeral key
 if 'DJANGO_SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-
 else:
     # Use if/else rather than a default value to avoid calculating this if we don't need it
     print("WARNING: DJANGO_SECRET_KEY not found in os.environ. Generating ephemeral SECRET_KEY.")
     SECRET_KEY = ''.join([random.SystemRandom().choice(string.printable) for i in range(50)])
 
-# Redirect all requests to HTTPS
-SECURE_SSL_REDIRECT = os.getenv('DJANGO_SECURE_SSL_REDIRECT', 'off') == 'on'
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+ALLOWED_HOSTS = ['reparacionesmaza.com']
 
-# Accept all hostnames, since we don't know in advance which hostname will be used for any given Heroku instance.
-# IMPORTANT: Set this to a real hostname when using this in production!
-# See https://docs.djangoproject.com/en/1.10/ref/settings/#allowed-hosts
-#ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(';')
-ALLOWED_HOSTS = ['*'] 
+# Database
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# BASE_URL required for notification emails
-# BASE_URL = 'http://' + os.getenv('BASE_URL') + ':8000'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-        },
-    },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rmazaweb',
+        'USER': 'rmazaweb',
+        'PASSWORD': 'rmazaweb',
+        'HOST': '',  # Set to empty string for localhost.
+        'PORT': '',  # Set to empty string for default.
+        'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
+    }
 }
